fa1836d4aa75dc3ab4e4626f6b3164b5469d0c30
shoc71 <supersonicwind6@gmail.com>
2025-01-09 21:00:19 -0500

    fixing virtual DOM

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import '../styles/Navbar.css';
-
+import { Link } from 'react-router-dom';
 
 const styles = {
   navbarStyle: {
     background: 'tan',
     justifyContent: 'flex-end',
+    display: 'flex', 
+    gap: '1rem', 
+    padding: '0.5rem', 
   },
 };
 
@@ -12,9 +15,10 @@ const styles = {
 function Navbar() {
   return (
     <nav style={styles.navbarStyle} className="navbar">
-        <a href="../App.jsx" target="_blank">Home</a>
-        <a href="./recipes.jsx" target="_blank">Recipes</a>
-        <a href="./login.jsx" target="_blank">Login</a>
+      {/* Use Link for internal navigation */}
+      <Link to="/">Home</Link>
+      <Link to="/recipes">Recipes</Link>
+      <Link to="/login">Login</Link>
     </nav>
   );
 }
